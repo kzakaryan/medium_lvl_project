52079c40e24f54bea1d63fd8e0626ac0c5ba78f5
kzakaryan <kzakaryan@griddynamics.com>
2024-12-30 02:50:37 +0400

    Deleting unnecessary files

--- viewer/src/test/java/MainTestIT.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-
-public class MainTestIT {
-
-    @Test
-    public void testIntegrationTest() {
-        System.out.println("Running Integration Test");
-    }
-}
